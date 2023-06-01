f6fb09a3a146fce7ffd0d71ac7c34281158059b9
Julian Forero <julianpajoy@outlook.com>
2023-05-31 20:24:26 -0500

    Componente examenes

--- cursos/src/app/components/examenes/examenes.component.ts
+++ cursos/src/app/components/examenes/examenes.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { CommonListarComponent } from '../common-listar.component';
+import { Examen } from 'src/app/models/examen';
+import { ExamenService } from 'src/app/services/examen.service';
 
 @Component({
   selector: 'app-examenes',
   templateUrl: './examenes.component.html',
   styleUrls: ['./examenes.component.css']
 })
-export class ExamenesComponent {
+export class ExamenesComponent extends CommonListarComponent<Examen, ExamenService> {
+
+  constructor(service: ExamenService){
+    super(service);
+    this.titulo = 'Listado de examenes';
+    this.nombreModel = Examen.name;
+  }
 
 }
--- cursos/src/app/services/examen.service.ts
+++ cursos/src/app/services/examen.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { ComunService } from './comun.service';
 import { Examen } from '../models/examen';
+import { BASE_ENDPOINT } from '../config/app';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ExamenService extends ComunService<Examen>{
 
-  protected override basePuntofinal = 'http://localhost:8090/api/examenes';
+  protected override basePuntofinal = BASE_ENDPOINT + '/examenes';
+
+  constructor(http: HttpClient) { 
+    super(http);
+  }
 }
